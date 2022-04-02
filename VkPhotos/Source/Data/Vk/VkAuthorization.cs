62c004045257e2163e68db762c8504d82113860b
Venz <venexzz@gmail.com>
2022-04-02 17:13:13 +0300

    Updated to VK API version to 5.131.

--- VkPhotos/Source/Data/Vk/VkAuthorization.cs
+++ VkPhotos/Source/Data/Vk/VkAuthorization.cs
@@ -5,6 +5,6 @@ namespace VkPhotos.Data.Vk
     public class VkAuthorization
     {
         public static String GetRedirectUrl() => "https://oauth.vk.com/blank.html";
-        public static Uri GetOAuthUri() => new Uri($"https://oauth.vk.com/authorize?client_id={PrivateData.VkAppId}&display=page&scope=photos,offline&response_type=token&v=5.42&redirect_uri={GetRedirectUrl()}");
+        public static Uri GetOAuthUri() => new Uri($"https://oauth.vk.com/authorize?client_id={PrivateData.VkAppId}&display=page&scope=photos,offline&response_type=token&v=5.131&redirect_uri={GetRedirectUrl()}");
     }
 }
